68a8955294939560a7a20befa036cf87d2be31cd
HendersonDaniel <danielhenderson.codeprojects@gmail.com>
2023-09-16 22:15:59 -0400

    added frontend

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 // Import dependencies
-const fs = require("fs");
-const XLSX = require("xlsx");
-const jsontoxml = require("jsontoxml");
+import * as XLSX from 'xlsx';
 
-var workbook = XLSX.utils.book_new();
 
-//basically you put a 2d array into this and it will write it into the excel sheet
-var worksheet = XLSX.utils.aoa_to_sheet([
-    ["A1", "B1", "C1"],
-    ["A2", "B2", "C2"],
-    ["A3", "B3", "C3"]
-  ]);
+// add an event listener to the form to handle submission
+document.addEventListener("DOMContentLoaded", function () {
 
-var new_name = XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1", true);
+  var inputElement = document.getElementById("new-task-title");
 
-//for this to work you need a excel file named "test.xlsx" in the same folder and it will edit that file
-XLSX.writeFile(workbook, "test.xlsx");
+  document.getElementById("new-task-form").addEventListener("submit", function (e) {
+    e.preventDefault(); // prevent the form from submitting and refreshing the page
+
+    // Get the user's input from the input element
+    var inputValue = inputElement.value;
+    
+
+
+
+    //workbook edit
+
+    console.log(XLSX.version);
+    var workbook = XLSX.utils.book_new();
+
+    //basically you put a 2d array into this and it will write it into the excel sheet
+    var worksheet = XLSX.utils.aoa_to_sheet(arr); //2d array into the xlsx
+    
+    var new_name = XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1", true);
+    
+    //for this to work you need a excel file named "test.xlsx" in the same folder and it will edit that file
+    XLSX.writeFile(workbook, "test.xlsx");
+    
+    
+
+  });
+});
